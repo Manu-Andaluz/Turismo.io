6f7460de66251e179d0e10d35c163df0696d25ba
Manu-Andaluz <manupeandaluz@gmail.com>
2022-07-20 22:11:56 -0300

    Adding new responsive designe

---
+++ script.js
@@ -0,0 +1,7 @@
+//NavBar
+const burger = document.getElementById('burger');
+const ulNavBar = document.getElementsByClassName('.container-links');
+
+burger.addEventListener('click',() => {
+    
+})
